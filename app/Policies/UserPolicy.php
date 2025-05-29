3785ff1dc82f0b7e5015b14f84ed8e5a445a58eb
AxelReviron <axel-reviron@proton.me>
2025-05-29 18:05:44 +0200

    UserPolicy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Utils\HandlesPolicies;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPolicy
+{
+    use HandlesPolicies;
+
+    protected string $model = User::class;
+
+    protected function isOwn(User $user, Model $model): bool
+    {
+        return $model instanceof User && $user->getKey() === $model->getKey();
+    }
+}
