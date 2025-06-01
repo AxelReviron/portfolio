104ac0395fa14cc78e7480d848d6233bd1fb2bc8
AxelReviron <axel-reviron@proton.me>
2025-06-01 20:42:25 +0200

    Add dev icons

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,15 +3,23 @@
 import { onMounted, PropType } from 'vue';
 import ProjectInterface from '@/interfaces/projectInterface';
 import HomeSection from '@/components/HomeSection.vue';
-import NavBar from '@/layouts/NavBar.vue';
 import TechnologySection from '@/components/TechnologySection.vue';
+import { Head } from '@inertiajs/vue3'
+
 
 const { projects } = defineProps({projects: Array as PropType<ProjectInterface[]>})
 
+onMounted(() => {
+    console.log({projects})
+})
 </script>
 
 <template>
-<!--    <NavBar/>-->
+    <Head>
+        <title>Axel Reviron</title>
+        <meta name="description" content="Fullstack Web Developer">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css">
+    </Head>
     <HomeSection/>
     <TechnologySection/>
 </template>
