13798fbd884590a5e76d1e95e44cfb8524032f3b
AxelReviron <axel-reviron@proton.me>
2025-06-01 20:41:24 +0200

    Title component

---
+++ resources/js/components/Title.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    'label': String,
+});
+</script>
+
+<template>
+    <h2 class="text-4xl md:text-4xl font-medium text-green-900 mb-4">
+        {{ props.label }}
+    </h2>
+</template>
+
+<style scoped>
+
+</style>
