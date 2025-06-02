0fd75ca62181556fe12563df04f8479566a732f9
AxelReviron <axel-reviron@proton.me>
2025-06-02 12:19:17 +0200

    Make new component

---
+++ resources/js/components/TechnologyCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+    label: String,
+    icon: String,
+    website: String
+});
+</script>
+
+<template>
+    <div class="border border-neutral-200 bg-neutral-50 shadow-sm rounded-lg py-1 px-2 hover:scale-105">
+        <a :href="props.website" class="text-sm inline">
+            <i v-if="props.icon" :class="props.icon" class="mr-2"></i>
+            {{ props.label }}
+        </a>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
