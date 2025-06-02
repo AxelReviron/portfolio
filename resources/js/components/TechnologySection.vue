fe4419f3665aee2163d5b20a91d042f7487aaedc
AxelReviron <axel-reviron@proton.me>
2025-06-02 19:26:16 +0200

    Get data from backend

--- resources/js/components/TechnologySection.vue
+++ resources/js/components/TechnologySection.vue
@@ -1,108 +1,16 @@
 <script setup lang="ts">
 import Title from '@/components/Title.vue';
-import TechnoCard from '@/components/TechnologyCategoryCard.vue';
+import TechnologyCategoryCard from '@/components/TechnologyCategoryCard.vue';
+import { PropType } from 'vue';
+import CategoryInterface from '@/interfaces/categoryInterface';
 
-const technoCardDatas = [// TODO: Get from backend
-    {
-        category: 'Frontend',
-        technologies: [
-            {
-                label: 'VueJS',
-                icon: 'devicon-vuejs-plain colored',
-                website: ''
-            },
-            {
-                label: 'Nuxt',
-                icon: 'devicon-nuxtjs-plain colored',
-                website: '/'
-            },
-            {
-                label: 'Tailwind CSS',
-                icon: 'devicon-tailwindcss-original colored',
-                website: '/'
-            },
-            {
-                label: 'Inertia',
-                icon: null,
-                website: '/'
-            },
-        ]
-    },
-    {
-        category: 'Backend',
-        technologies: [
-            {
-                label: 'Laravel',
-                icon: 'devicon-laravel-original colored',
-                website: '/'
-            },
-            {
-                label: 'Node.js',
-                icon: 'devicon-nodejs-plain-wordmark colored',
-                website: '/'
-            },
-            {
-                label: 'MySQL',
-                icon: 'devicon-mysql-original colored',
-                website: '/'
-            },
-        ]
-    },
-    {
-        category: 'Tools & Services',
-        technologies: [
-            {
-                label: 'Git',
-                icon: 'devicon-git-plain colored',
-                website: '/'
-            },
-            {
-                label: 'Docker',
-                icon: 'devicon-docker-plain colored',
-                website: '/'
-            },
-            {
-                label: 'Linux',
-                icon: 'devicon-linux-plain colored',
-                website: '/'
-            },
-            {
-                label: 'Bash',
-                icon: 'devicon-bash-plain colored',
-                website: '/'
-            },
-            {
-                label: 'Penpot',
-                icon: null,
-                website: '/'
-            },
-        ]
-    },
-    {
-        category: 'Videos Games & Others',
-        technologies: [
-            {
-                label: 'Godot',
-                icon: 'devicon-godot-plain colored',
-                website: '/'
-            },
-            {
-                label: 'Blender',
-                icon: 'devicon-blender-original colored',
-                website: '/'
-            },
-            {
-                label: 'FL Studio',
-                icon: null,
-                website: '/'
-            },
-        ]
-    },
-];
+const { categories } = defineProps({
+    categories: Array as PropType<CategoryInterface[]>
+});
 </script>
 
 <template>
-    <div id="technologies" class="min-h-screen flex justify-center items-center relative overflow-hidden bg-green-100">
+    <div id="technologies" class="py-24 md:py-42 flex justify-center items-center relative overflow-hidden bg-green-50">
         <div class="flex flex-col justify-center items-center w-300">
             <Title label="Technologies"/>
             <p class="text-lg md:text-xl text-neutral-600 text-center w-sm px-4 md:px-0 md:w-2xl mb-8 leading-relaxed">
@@ -111,10 +19,10 @@ const technoCardDatas = [// TODO: Get from backend
 
             <div class="flex flex-wrap flex-row gap-4 justify-center">
                 <div
-                    :key="technoCardData.category"
-                    v-for="technoCardData in technoCardDatas"
+                    :key="category.name"
+                    v-for="category in categories"
                 >
-                    <TechnoCard :category="technoCardData.category" :technologies="technoCardData.technologies"/>
+                    <TechnologyCategoryCard :category="category"/>
                 </div>
             </div>
         </div>
