853f8920c390ff0cad59d54ba8980759f6e94cb7
AxelReviron <axel-reviron@proton.me>
2025-06-02 19:23:36 +0200

    Add experience section

---
+++ resources/js/components/ExperienceSection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Title from '@/components/Title.vue';
+import { onMounted, PropType } from 'vue';
+import ExperienceInterface from '@/interfaces/experienceInterface';
+
+const { experiences } = defineProps({
+    experiences: Array as PropType<ExperienceInterface[]>,
+})
+
+onMounted(() => {
+    console.log(experiences)
+})
+</script>
+
+<template>
+
+    <div id="experiences" class="py-42 flex justify-center items-center relative overflow-hidden bg-white">
+        <div class="flex flex-col justify-center items-center w-300">
+            <Title label="Experience"/>
+            <p class="text-lg md:text-xl text-neutral-600 text-center w-sm px-4 md:px-0 md:w-2xl mb-8 leading-relaxed">
+                lorem ipsum
+            </p>
+
+
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+</style>
