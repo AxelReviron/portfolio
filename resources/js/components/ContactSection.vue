c5df06cf91eee4e239834b3f70de004c72a35728
AxelReviron <axel-reviron@proton.me>
2025-06-02 19:23:20 +0200

    Add contact section

---
+++ resources/js/components/ContactSection.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Title from '@/components/Title.vue';
+
+</script>
+
+<template>
+
+    <div id="contact" class="py-42 flex justify-center items-center relative overflow-hidden bg-white">
+        <div class="flex flex-col justify-center items-center w-300">
+            <Title label="Contact"/>
+            <p class="text-lg md:text-xl text-neutral-600 text-center w-sm px-4 md:px-0 md:w-2xl mb-8 leading-relaxed">
+                lorem ipsum
+            </p>
+
+
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+</style>
