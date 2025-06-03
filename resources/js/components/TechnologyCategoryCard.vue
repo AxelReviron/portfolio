97eaaac768b880b82af9bc70d6d03eb10e03b219
AxelReviron <axel-reviron@proton.me>
2025-06-03 17:23:23 +0200

    animations + delay

--- resources/js/components/TechnologyCategoryCard.vue
+++ resources/js/components/TechnologyCategoryCard.vue
@@ -1,33 +1,31 @@
 <script setup lang="ts">
 
-import { onMounted, PropType, shallowRef, useTemplateRef } from 'vue';
-import { useIntersectionObserver } from '@vueuse/core';
+import { PropType, ref } from 'vue';
 import CategoryInterface from '@/interfaces/categoryInterface';
 import TechnologyCard from '@/components/TechnologyCard.vue';
+import useItemOnceInView from '@/composables/useItemOnceInView';
 
-const { category } = defineProps({
-    category: Object as PropType<CategoryInterface>
+const { category, delay } = defineProps({
+    category: Object as PropType<CategoryInterface>,
+    delay: Number
 });
 
-const target = useTemplateRef<HTMLDivElement>('target')
-const targetIsVisible = shallowRef(false)
+const target = ref<HTMLDivElement | null>(null);
+const targetIsVisible = ref(false);
 
-onMounted(() => {
-    useIntersectionObserver(target, ([entry]) => {
-            targetIsVisible.value = entry?.isIntersecting || false
-        },
-    )
-})
+useItemOnceInView(target, targetIsVisible);
+
 </script>
 
 <template>
     <div
-        ref="target"
-        class="bg-white shadow-sm px-4 py-2 rounded-lg w-84 h-42 duration-400 ease-in"
+        :ref="(element) => { if (element) target = element as HTMLDivElement; }"
+        class="bg-white shadow-sm px-4 py-2 rounded-lg w-84 h-42 duration-600 ease-in"
         :class="{
             'opacity-0 translate-y-4': !targetIsVisible,
             'opacity-100 translate-y-0': targetIsVisible
         }"
+        :style="{ 'transition-delay': `${delay}ms` }"
     >
         <h2 class="text-xl md:text-2xl font-medium text-green-900 mb-4">
             {{ category.name }}
