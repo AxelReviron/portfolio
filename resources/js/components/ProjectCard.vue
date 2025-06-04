62f3eb23bbc46d5b84b87d46cb5fab26ebb80215
AxelReviron <axel-reviron@proton.me>
2025-06-04 17:40:29 +0200

    polish project section

---
+++ resources/js/components/ProjectCard.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ExternalLink } from 'lucide-vue-next';
+import { PropType } from 'vue';
+import ProjectInterface from '@/interfaces/projectInterface';
+import TechnologyCard from '@/components/TechnologyCard.vue';
+
+const { project } = defineProps({
+    project: Object as PropType<ProjectInterface>,
+})
+</script>
+
+<template>
+    <div class="w-80 md:w-100 flex flex-col bg-white rounded-xl shadow-lg border border-white/50 overflow-hidden">
+        <div class="relative w-full h-50 overflow-hidden rounded-t-xl group mb-4">
+            <img
+                v-if="project.media && project.media.length > 0 && project.media[0].original_url"
+                :src="project.media[0].original_url"
+                alt="Project Cover"
+                class="absolute inset-0 w-full h-full object-cover
+                       transition-all duration-300
+                       group-hover:blur-sm"
+            >
+            <p
+                class="absolute inset-0 flex items-center justify-center p-4
+                       text-white text-center text-lg md:text-xl font-medium
+                       text-shadow-lg opacity-0 transition-opacity duration-300
+                       group-hover:opacity-100 cursor-pointer"
+            >{{ project.description }}</p>
+        </div>
+
+        <div class="px-4 mb-4 md:h-38 flex flex-col justify-start">
+            <h3 class="text-xl text-green-700 font-bold mb-4">{{ project.name }}</h3>
+            <p class="md:hidden mb-2">{{ project.description }}</p>
+            <ul
+                v-if="project.technologies"
+                class="flex flex-row gap-2 flex-wrap mb-2"
+            >
+                <li
+                    v-for="technology in project.technologies"
+                    :key="technology.name"
+                >
+                    <TechnologyCard :technology="technology" />
+                </li>
+            </ul>
+            <div class="flex flex-row gap-4 mt-auto">
+                <a
+                    v-if="project.website"
+                    :href="project.website"
+                    target="_blank"
+                    class="flex flex-row gap-2 text-lg text-green-600 font-medium"
+                >
+                    Website
+                    <ExternalLink class="mt-1 w-5 h-5"/>
+                </a>
+                <a
+                    v-if="project.github_url"
+                    :href="project.github_url"
+                    target="_blank"
+                    class="flex flex-row gap-2 text-lg text-green-600 font-medium"
+                >
+                    Github
+                    <ExternalLink class="mt-1 w-5 h-5"/>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/components/ProjectSection.vue
+++ resources/js/components/ProjectSection.vue
@@ -1,27 +1,47 @@
 <script setup lang="ts">
 import Title from '@/components/Title.vue';
-import { onMounted, PropType } from 'vue';
+import { PropType, ref } from 'vue';
 import ProjectInterface from '@/interfaces/projectInterface';
+import ProjectCard from '@/components/ProjectCard.vue';
+import useOnceInView from '@/composables/useItemsOnceInView';
 
 const { projects } = defineProps({
     projects: Array as PropType<ProjectInterface[]>,
 })
 
-onMounted(() => {
-    console.log(projects)
-})
+const projectRefs = ref<HTMLElement[]>([]);
+const itemHasBeenSeen = ref<boolean[]>([]);
+
+const baseDelay = 100;
+const delayIncrement = 150;
+
+useOnceInView(projectRefs, itemHasBeenSeen);
 </script>
 
 <template>
 
-    <div id="projects" class="py-42 flex justify-center items-center relative overflow-hidden bg-green-50">
-        <div class="flex flex-col justify-center items-center w-300">
+    <div id="projects" class="py-24 md:py-42 flex justify-center items-center relative overflow-hidden bg-green-50">
+        <div class="flex flex-col justify-center items-center w-400">
             <Title label="Projects"/>
             <p class="text-lg md:text-xl text-neutral-600 text-center w-sm px-4 md:px-0 md:w-2xl mb-8 leading-relaxed">
-                lorem ipsum
+                A selection of my recent work, showcasing my expertise in full-stack development and problem-solving abilities.
             </p>
+            <ul class="flex justify-center flex-wrap flex-row gap-8">
+               <li
+                   v-for="(project, index) in projects"
+                   :key="project.name"
+                   :ref="(element) => { if (element) projectRefs[index] = element as HTMLElement; }"
+                   class="duration-600 ease-in"
+                   :class="{
+                       'opacity-0 translate-y-4': !itemHasBeenSeen[index],
+                       'opacity-100 translate-y-0': itemHasBeenSeen[index]
+                   }"
+                   :style="{ 'transition-delay': `${baseDelay + (index * delayIncrement)}ms` }"
+               >
+                   <ProjectCard :project="project"/>
+               </li>
+            </ul>
         </div>
-
     </div>
 </template>
 
