b9f16e841618c87e7540efbf289502c709798061
AxelReviron <axel-reviron@proton.me>
2025-06-04 11:55:30 +0200

    Update ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -9,21 +9,48 @@ use Illuminate\Database\Seeder;
 
 class ProjectSeeder extends Seeder
 {
+    private array $projects = [
+        [
+            'name' => 'Portfolio',
+            'description' => 'Personal portfolio',
+            'website' => 'https://axel-reviron.fr',
+            'github_url' => 'https://github.com/AxelReviron/portfolio',
+            'technologies' => ['TypeScript', 'VueJS', 'Laravel', 'Inertia', 'Tailwind']
+        ],
+        [
+            'name' => 'Quentin Salomon',
+            'description' => 'Portfolio of Quentin Salomon, a video editor.',
+            'website' => 'https://quentin-salomon.fr/',
+            'github_url' => null,
+            'technologies' => ['TypeScript', 'VueJS', 'Laravel', 'Inertia', 'Tailwind']
+        ],
+        [
+            'name' => 'Hubspot Supercharger',
+            'description' => 'Browser extension for HubSpot, add custom logos, distinct color schemes, and menu navigation customization. ',
+            'website' => 'https://quentin-salomon.fr/',
+            'github_url' => null,
+            'technologies' => ['TypeScript', 'Plasmo', 'Node.js']
+        ],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Project::factory()->create([
-            'name' => 'Portfolio',
-            'description' => 'Personal portfolio',
-            'website' => 'https://axel-reviron.fr',
-            'github_url'=> 'https://github.com/AxelReviron/portfolio',
-        ])->technologies()->attach([
-            Technology::where('name', 'VueJS')->first()->getKey(),
-            Technology::where('name', 'Laravel')->first()->getKey(),
-            Technology::where('name', 'Inertia')->first()->getKey(),
-            Technology::where('name', 'Tailwind')->first()->getKey(),
-        ]);
+        foreach ($this->projects as $projectDatas) {
+            $project = Project::factory()->create([
+                'name' => $projectDatas['name'],
+                'description' => $projectDatas['description'],
+                'website' => $projectDatas['website'],
+                'github_url'=> $projectDatas['github_url'],
+            ]);
+
+            $technologyIds = Technology::whereIn('name', $projectDatas['technologies'])
+                ->pluck('id')
+                ->toArray();
+
+            $project->technologies()->attach($technologyIds);
+        }
     }
 }
