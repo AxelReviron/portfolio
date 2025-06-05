03d6667c953c8bf001b076b0c5a840eb5fed4d6e
AxelReviron <axel-reviron@proton.me>
2025-06-05 17:37:18 +0200

    animate bullet point and timeline

--- resources/js/components/ExperienceSection.vue
+++ resources/js/components/ExperienceSection.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import Title from '@/components/Title.vue';
-import { PropType, ref } from 'vue';
+import { onMounted, PropType, ref } from 'vue';
 import ExperienceInterface from '@/interfaces/experienceInterface';
 import { ExternalLink } from 'lucide-vue-next';
 import useOnceInView from '@/composables/useItemsOnceInView';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const { experiences } = defineProps({
     experiences: Array as PropType<ExperienceInterface[]>,
@@ -11,6 +16,8 @@ const { experiences } = defineProps({
 
 const experienceRefs = ref<HTMLElement[]>([]);
 const itemHasBeenSeen = ref<boolean[]>([]);
+
+const bulletPointRefs = ref<HTMLElement[]>([]);
 
 const baseDelay = 100;
 const delayIncrement = 150;
@@ -37,14 +44,53 @@ function formatExperiencePeriod(startDateString: string, endDateString: string |
     const formattedEnd = endDate.toLocaleString('en-US', options);
     return `${formattedStart} - ${formattedEnd}`;
 }
+
+function animateTimelineBar() {
+    gsap.set('.timeline', { height: 0 });
+    gsap.to('.timeline', {
+        height: '100%',
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.list-wrapper',
+            start: "top center",
+            end: "bottom center",
+            scrub: true,
+        },
+    });
+}
+
+function animateBulletPoint() {
+    bulletPointRefs.value.forEach((bulletPointElement) => {
+        if (bulletPointElement) {
+            gsap.to(bulletPointElement, {
+                opacity: 1,
+                duration: 0.3,
+                ease: 'power1.out',
+                scrollTrigger: {
+                    trigger: bulletPointElement,
+                    start: 'top center+=25px',
+                    toggleActions: 'play none none reverse',
+                }
+            });
+        }
+    });
+}
+
+onMounted(() => {
+    animateTimelineBar();
+    animateBulletPoint();
+});
 </script>
 
 <template>
 
     <div id="experiences" class="py-24 md:py-42 flex justify-center items-center relative overflow-hidden bg-white">
         <div class="flex flex-col justify-center items-center w-300">
-            <Title label="Experiences"/>
-            <ul class="mx-6 md:mr-0 mt-8 border-l-5 border-green-200 flex flex-col justify-start">
+            <Title label="Experiences" />
+            <div class="list-wrapper relative">
+                <div class="timeline absolute bg-green-200 w-2 top-10 left-[1.15rem] rounded-xl"></div>
+                <ul class="mx-6 md:mr-0 mt-8 flex flex-col justify-start">
+
                 <li
                     v-for="(experience, index) in experiences"
                     :key="experience.name"
@@ -56,8 +102,11 @@ function formatExperiencePeriod(startDateString: string, endDateString: string |
                     }"
                     :style="{ 'transition-delay': `${baseDelay + (index * delayIncrement)}ms` }"
                 >
-                    <div class="absolute -left-11 top-1.5 w-5 h-5 bg-green-700 rounded-full border-2 border-white"></div>
-                    <h3 class="text-xl text-green-700 font-bold mb-2">
+                    <div
+                        class="bullet-point opacity-0 absolute -left-11 top-1.5 w-5 h-5 bg-green-700 rounded-full border-2 border-white"
+                        :ref="(element) => { if (element) bulletPointRefs[index] = element as HTMLElement; }"
+                    ></div>
+                    <h3 class="text-2xl text-green-700 font-bold mb-2">
                         {{ formatExperiencePeriod(experience.start_date, experience.end_date) }}
                     </h3>
                     <div class="flex flex-row gap-2">
@@ -71,7 +120,7 @@ function formatExperiencePeriod(startDateString: string, endDateString: string |
                     <p v-html="experience.description" class="md:w-200 text-lg md:text-justify"></p>
                 </li>
             </ul>
-
+            </div>
         </div>
 
     </div>
