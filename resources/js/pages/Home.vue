334e12daf85453177f23def760ba0c53c5d334e7
AxelReviron <axel-reviron@proton.me>
2025-06-06 21:03:11 +0200

    Fix Plausible

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -25,8 +25,7 @@ const { categories, experiences, projects } = defineProps({
         <meta name="description" content="Fullstack Web Developer">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css" />
-        <script defer data-domain="axel-reviron.fr" src="https://plausible.nizukameha.xyz/js/script.hash.outbound-links.pageview-props.tagged-events.js"></script>
-        <script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
+        <script defer data-domain="axel-reviron.fr" src="https://plausible.nizukameha.xyz/js/script.js"></script>
     </Head>
     <HomeSection/>
     <TechnologySection :categories="categories.data"/>
